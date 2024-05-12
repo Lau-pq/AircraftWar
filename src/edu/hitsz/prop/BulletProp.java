db7ae705b3e17912898ccb328f49fd449858b8a6
Lpq <1322325183@qq.com>
2024-05-12 13:43:23 +0800

    refactor: 用同步锁的方式解决多线程写同一共享变量问题

--- src/edu/hitsz/prop/BulletProp.java
+++ src/edu/hitsz/prop/BulletProp.java
@@ -5,41 +5,51 @@ import edu.hitsz.application.MusicManager;
 import edu.hitsz.strategy.ShootStrategy;
 import edu.hitsz.strategy.StraightShootStrategy;
 
-public class BulletProp extends AbstractProp {
+public abstract class BulletProp extends AbstractProp {
     public BulletProp(int locationX, int locationY, int speedX, int speedY) {
         super(locationX, locationY, speedX, speedY);
     }
 
-    private static Thread currentThread;
     protected int shootNum;
     protected ShootStrategy shootStrategy;
     protected int activeTime;
+
+    private static int bulletPropNum = 0;
+
+    private static synchronized void changeBulletProp(String event) {
+        switch (event) {
+            case "increase" : bulletPropNum += 1;
+            case "decrease" : bulletPropNum -= 1;
+            default:
+        }
+    }
 
     public void recoverStraightShoot (HeroAircraft heroAircraft) {
         // 恢复单射
         Runnable r = () -> {
             try {
                 Thread.sleep(activeTime);
-                heroAircraft.setShootNum(1);
-                heroAircraft.setShootStrategy(new StraightShootStrategy());
-            } catch (InterruptedException ignored) {
+                if (bulletPropNum == 1) {
+                    heroAircraft.setShootNum(1);
+                    heroAircraft.setShootStrategy(new StraightShootStrategy());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                changeBulletProp("decrease");
             }
         };
-        if (currentThread != null) {
-            currentThread.interrupt();
-        }
-        currentThread = new Thread(r);
-        currentThread.start();
+        new Thread(r).start();
     }
 
     @Override
     public void activate(HeroAircraft heroAircraft) {
+        changeBulletProp("increase");
         heroAircraft.setShootNum(shootNum);
         heroAircraft.setShootStrategy(shootStrategy);
         recoverStraightShoot(heroAircraft);
         System.out.println("FireSupply active!");
         MusicManager.action("supply");
     }
-
 
 }
